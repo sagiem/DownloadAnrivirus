be014b3aaa818811ad9d55e44c3af2d5ac4851d2
sagiem <sagiems@gmail.com>
2020-09-03 20:23:50 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            var webclient = new WebClient();
+            webclient.Proxy = null;
+            webclient.DownloadFile(new Uri("https://devbuilds.s.kaspersky-labs.com/devbuilds/KVRT/latest/full/KVRT.exe"), "kaspersky.exe");
+            webclient.DownloadFile(new Uri("http://www.freedrweb.com/download+cureit/gr/?lng=ru"), "DrWeb.exe");
+        }
+    }
+}
